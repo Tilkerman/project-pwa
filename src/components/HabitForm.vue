10a3cbbd8d5877512b5a438826400021473155ba
Васильев Евгений Викторович <z108@next.int>
2025-12-03 10:44:13 +0400

    fix: use props.habit instead of habit in template

--- src/components/HabitForm.vue
+++ src/components/HabitForm.vue
@@ -168,7 +168,7 @@
 
     <div class="form-actions">
       <button
-        v-if="isEditing"
+        v-if="props.habit"
         type="button"
         class="btn-delete-icon"
         @click.stop="showDeleteConfirm = true"
