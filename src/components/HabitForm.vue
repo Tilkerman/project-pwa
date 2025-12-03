6a9664ea3aa45be83b11af3a481e480fafb1ee18
Васильев Евгений Викторович <z108@next.int>
2025-12-03 10:26:28 +0400

    fix: use props.habit for delete button condition

--- src/components/HabitForm.vue
+++ src/components/HabitForm.vue
@@ -168,7 +168,7 @@
 
     <div class="form-actions">
       <button
-        v-if="habit"
+        v-if="props.habit"
         type="button"
         class="btn-delete-icon"
         @click.stop="showDeleteConfirm = true"
