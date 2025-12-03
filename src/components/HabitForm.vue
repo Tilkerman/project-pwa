dcff021ab28f20ade52ade9c41c0f9834c83d7a8
Васильев Евгений Викторович <z108@next.int>
2025-12-03 10:25:56 +0400

    fix: use habit prop directly for delete button visibility

--- src/components/HabitForm.vue
+++ src/components/HabitForm.vue
@@ -168,7 +168,7 @@
 
     <div class="form-actions">
       <button
-        v-if="isEditing"
+        v-if="habit"
         type="button"
         class="btn-delete-icon"
         @click.stop="showDeleteConfirm = true"
